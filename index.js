78b366032d49dcc6553ce4ab9f4302b1515540ca
yijiejiang <yijiejiang@tencent.com>
2023-03-22 11:03:32 +0800

    chore

--- index.js
+++ index.js
@@ -57,6 +57,6 @@ app.get("/wxad-design-ai", async (req, res) => {
   }
 })
 
-app.listen(3000, () => {
-  console.log("Server listening on port 3000")
+app.listen(8080, () => {
+  console.log("Server listening on port 8080")
 })
